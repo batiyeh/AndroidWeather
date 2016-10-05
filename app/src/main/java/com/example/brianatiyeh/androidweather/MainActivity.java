2c6f44fa3a662db4da9e9b50867d6f980cc5a1bc
Brian Atiyeh <btatiyeh@gmail.com>
2016-10-04 20:36:02 -0400

    Added Project

---
+++ app/src/main/java/com/example/brianatiyeh/androidweather/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.brianatiyeh.androidweather;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ListView;
+
+import static com.example.brianatiyeh.androidweather.R.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setTitleTextColor(Color.WHITE);
+        ListView listView = (ListView) findViewById(id.listView);
+
+
+
+    }
+}
