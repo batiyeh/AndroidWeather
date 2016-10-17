11c04737015d936bf3ff5f56a207a138c53247ca
Brian Atiyeh <btatiyeh@gmail.com>
2016-10-16 21:54:44 -0400

    Final

--- app/src/main/java/com/example/brianatiyeh/androidweather/MainActivity.java
+++ app/src/main/java/com/example/brianatiyeh/androidweather/MainActivity.java
@@ -1,31 +1,101 @@
 package com.example.brianatiyeh.androidweather;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.ListView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
 import static com.example.brianatiyeh.androidweather.R.id;
 import static com.example.brianatiyeh.androidweather.R.layout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private static final int PERMISSION_REQUEST = 1;
+    private GoogleApiClient googleApiClient;
+    private ForecastAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_main);
 
+        if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[] {Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST);
+        }
+
         Toolbar toolbar = (Toolbar) findViewById(id.toolbar);
         toolbar.setTitle(R.string.app_name);
         toolbar.setTitleTextColor(Color.WHITE);
 
-        ForecastAdapter adapter = new ForecastAdapter(this, R.layout.list_item);
+        adapter = new ForecastAdapter(this, R.layout.list_item);
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(adapter);
 
-        RetrieveForecast task = new RetrieveForecast(adapter);
-        task.execute(51.5072, -0.1275);
+        /*RetrieveForecast task = new RetrieveForecast(adapter);
+        task.execute(51.5072, -0.1275);*/
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API);
+                .build();
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(googleApiClient.isConnected()){
+            googleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode){
+            case PERMISSION_REQUEST:
+                /*if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                } else{
+
+                }*/
+                break;
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        if (lastLocation != null){
+            RetrieveForecast task = new RetrieveForecast(adapter);
+            task.execute(lastLocation.getLatitude(), lastLocation.getLongitude());
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        googleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 }
